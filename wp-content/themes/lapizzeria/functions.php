780766200c04f447b47bfc5b43f74b02bc3cbb3e
rayGreycloud <rayGreycloud@gmail.com>
2017-09-26 12:52:48 -0700

    Add normalize.css

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -3,9 +3,11 @@
 
 function lapizzeria_styles() {
   // adding stylesheets
-  wp_register_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+  wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0' );
+  wp_register_style( 'style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
 
   // Enqueue style
+  wp_enqueue_style( 'normalize');
   wp_enqueue_style( 'style');
 
 }
