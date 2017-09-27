32f9f754dc525c0f0def47df5314b24a904b578f
rayGreycloud <rayGreycloud@gmail.com>
2017-09-27 15:47:46 -0700

    Add styles to make menu responsive

--- wp-content/themes/lapizzeria/header.php
+++ wp-content/themes/lapizzeria/header.php
@@ -41,6 +41,9 @@
       </div><!--.container-->
     </header>
     <div class="main-menu">
+      <div class="mobile-menu">
+        <a href="#" class="mobile"><i class="fa fa-bars"></i> Menu</a>
+      </div>
       <div class="navigation container">
         <?php
           $args = array(
