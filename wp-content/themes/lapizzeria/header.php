0a1e14f41d0416eb97cfa96e649de13422228e0e
rayGreycloud <rayGreycloud@gmail.com>
2017-09-26 17:28:09 -0700

    Add global styles

--- wp-content/themes/lapizzeria/header.php
+++ wp-content/themes/lapizzeria/header.php
@@ -9,22 +9,36 @@
   <body>
 
     <header class="site-header">
-      <div class="logo">
-        <a href="<?php echo esc_url(home_url('/')); ?>">
-          <img
-          src="<?php echo get_template_directory_uri() ?>/img/logo.svg"
-          class="logo__image"
-          >
-        </a>
-      </div><!--.logo -->
-      <div class="header-information">
-        <div class="social-menu">
-          <div class="address">
-            <p>6708 NE Sandy Blvd, Portland, OR 97213</p>
-            <p>Phone: (971) 570-4496</p>
-          </div>
-        </div><!--.social-->
-      </div><!--.header-information-->
+      <div class="container">
+
+        <div class="logo">
+          <a href="<?php echo esc_url(home_url('/')); ?>">
+            <img
+            src="<?php echo get_template_directory_uri() ?>/img/logo.svg"
+            class="logo__image"
+            >
+          </a>
+        </div><!--.logo -->
+        <div class="header-information">
+          <div class="social-menu">
+            <?php
+            $args = array(
+              'theme_location' => 'social-menu',
+              'container' =>   'nav',
+              'container_class' =>  'social-menu',
+              'container_id' => 'social-menu',
+              'link_before' =>  '<span class="sr-text">',
+              'link_after' =>   '</span>'
+            );
+            wp_nav_menu($args);
+             ?>
+            <div class="address">
+              <p>6708 NE Sandy Blvd, Portland, OR 97213</p>
+              <p>Phone: (971) 570-4496</p>
+            </div>
+          </div><!--.social-->
+        </div><!--.header-information-->
+      </div><!--.container-->
     </header>
     <div class="main-menu">
       <div class="navigation">
