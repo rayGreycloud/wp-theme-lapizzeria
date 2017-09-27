645ef841792052d03bdc92ad39d918a3ad82da67
rayGreycloud <rayGreycloud@gmail.com>
2017-09-27 16:52:59 -0700

    Add google fonts

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -3,6 +3,7 @@
 
 function lapizzeria_styles() {
   // adding stylesheets
+  wp_register_style('googlefont', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway:400,700,900', array(), '1.0.0');
   wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0' );
   wp_register_style( 'fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0' );
   wp_register_style( 'style', get_template_directory_uri() . '/style.css', array('normalize', 'fontawesome'), '1.0' );
@@ -10,6 +11,7 @@ function lapizzeria_styles() {
   // Enqueue style
   wp_enqueue_style( 'normalize');
   wp_enqueue_style( 'fontawesome');
+  wp_enqueue_style( 'googlefont');
   wp_enqueue_style( 'style');
 
   // add javascript
