2c50980d5812f4d85c981cfad0a5b799e4eb0a07
rayGreycloud <rayGreycloud@gmail.com>
2017-09-29 08:29:37 -0700

    Add custom post type pizzas to dashboard

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -36,4 +36,46 @@ function lapizzeria_menus() {
 }
 
 add_action('init', 'lapizzeria_menus');
+
+function lapizzeria_specialities() {
+  $labels = array(
+    'name'                => _x('Pizzas', 'lapizzeria'),
+    'singular_name'       => _x('Pizzas', 'post type singular name','lapizzeria'),
+    'menu_name'           => _x('Pizzas', 'admin menu','lapizzeria'),
+    'name_admin_bar'      => _x('Pizzas', 'add new on admin bar','lapizzeria'),
+    'add_new'             => _x('Add New', 'book','lapizzeria'),
+    'add_new_item'        => __('Add New Pizza', 'lapizzeria'),
+    'new_item'            => __('New Pizzas', 'lapizzeria'),
+    'edit_item'           => __('Edit Pizzas', 'lapizzeria'),
+    'view_item'           => __('View Pizzas', 'lapizzeria'),
+    'all_items'           => __('All Pizzas', 'lapizzeria'),
+    'search_items'        => __('Search Pizzas', 'lapizzeria'),
+    'parent_item_colon'   => __('Parent Pizzas', 'lapizzeria'),
+    'not_found'           => __('No pizzas found.', 'lapizzeria'),
+    'not_found_in_trash'  => __('No pizzas found in trash.', 'lapizzeria')
+  );
+
+  $args = array(
+    'labels'              => $labels,
+    'description'         => __('Description', 'lapizzeria'),
+    'public'             => true,
+    'publicly_queryable' => true,
+    'show_ui'            => true,
+    'show_in_menu'       => true,
+    'query_var'          => true,
+    'rewrite'            => array('slug' => 'specialities'),
+    'capability_type'    => 'post',
+    'has_archive'        => true,
+    'hierarchical'       => false,
+    'menu_position'      => 6,
+    'supports'           => array('title', 'editor', 'thumbnail'),
+    'taxonomies'         => array('category'),
+  );
+
+  register_post_type('specialities', $args);
+}
+
+add_action('init', 'lapizzeria_specialities');
+
+
  ?>
