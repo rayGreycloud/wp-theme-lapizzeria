45fe9b5646d004cf1ef35d5ffc429e7dcd2e0e0a
rayGreycloud <rayGreycloud@gmail.com>
2017-09-30 13:23:11 -0700

    Display posts on blog

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -4,9 +4,7 @@
   $blog_page = get_option('page_for_posts');
   $imageId = get_post_thumbnail_id($blog_page);
   $image = wp_get_attachment_image_src($imageId, 'full');
-
  ?>
-
 
 <div class="hero" style="background-image:url(<?php echo $image[0]; ?>)">
   <div class="hero-content">
@@ -17,15 +15,39 @@
 </div>
 
 <div class="main-content container">
-  <main class="text-center content-text">
-  <?php while(have_posts()): the_post(); ?>
+  <div class="container-grid">
+    <main class="content-text columns2-3">
+    <?php while(have_posts()): the_post(); ?>
+      <article class="entry">
+        <a href="<?php the_permalink(); ?>">
+          <?php  the_post_thumbnail('specialties'); ?>
+        </a>
+        <header class="entry-header clear">
+          <div class="date">
+            <time>
+              <?php echo the_time('d'); ?>              <span><?php echo the_time('M') ?></span>
+            </time>
+          </div>
+          <div class="entry-title">
+            <?php the_title('<h2>', '</h2>'); ?>
+            <p class="author">
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <?php the_author(); ?>
+            </p>
+          </div>
+        </header>
+        <div class="entry-content">
+          <?php the_excerpt(); ?>
+          <a href="<?php the_permalink(); ?>"
+            class="button primary"
+          >Read More</a>
+        </div>
+      </article>
 
+    <?php endwhile; ?>
 
-
-  <?php endwhile; ?>
-
-
-  </main>
+    </main>
+  </div>
 </div>
 
 <?php get_footer(); ?>
