a76a80e288528f885924dc23fbc412b82317ff28
rayGreycloud <rayGreycloud@gmail.com>
2017-09-30 11:28:15 -0700

    Scaffold blog page

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,3 +1,31 @@
 <?php get_header(); ?>
-  <h1>This is index.php</h1>
+
+<?php
+  $blog_page = get_option('page_for_posts');
+  $imageId = get_post_thumbnail_id($blog_page);
+  $image = wp_get_attachment_image_src($imageId, 'full');
+
+ ?>
+
+
+<div class="hero" style="background-image:url(<?php echo $image[0]; ?>)">
+  <div class="hero-content">
+    <div class="hero-text">
+      <h2><?php echo get_the_title($blog_page); ?></h2>
+    </div>
+  </div>
+</div>
+
+<div class="main-content container">
+  <main class="text-center content-text">
+  <?php while(have_posts()): the_post(); ?>
+
+  <?php the_content(); ?>
+
+  <?php endwhile; ?>
+
+
+  </main>
+</div>
+
 <?php get_footer(); ?>
