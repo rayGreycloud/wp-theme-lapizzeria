d11d23b77ab78b6cf775f7684b6758a6471cd028
rayGreycloud <rayGreycloud@gmail.com>
2017-10-03 09:31:31 -0700

    Add styles to reservation form

--- wp-content/themes/lapizzeria/page-reservation.php
+++ wp-content/themes/lapizzeria/page-reservation.php
@@ -14,29 +14,31 @@ get_header(); ?>
   </div>
 </div>
 
-<div class="main-content container">
-  <main class="text-center content-text clear">
-    <form class="reservation-form" method="post">
-      <h2>Make a reservation</h2>
-      <div class="field">
-        <input type="text" name="name" placeholder="Name" required>
-      </div>
-      <div class="field">
-        <input type="datetime-local" name="date" placeholder="Date" required>
-      </div>
-      <div class="field">
-        <input type="email" name="email" placeholder="Email" required>
-      </div>
-      <div class="field">
-        <input type="tel" name="phone" placeholder="Phone Number" required>
-      </div>
-      <div class="field">
-        <textarea name="messgae" placeholder="Message" required></textarea>
-      </div>
-      <input type="submit" name="submit" class="button">
-      <input type="hidden" name="hidden" value=1>
+<div class="main-content container reservation">
+  <main class="content-text clear">
+    <div class="reservation-info">
 
-      </div>
+      <form class="reservation-form" method="post">
+        <h2>Make a reservation</h2>
+        <div class="field">
+          <input type="text" name="name" placeholder="Name" required>
+        </div>
+        <div class="field">
+          <input type="datetime-local" name="date" placeholder="Date" required>
+        </div>
+        <div class="field">
+          <input type="email " name="email" placeholder="Email" required>
+        </div>
+        <div class="field">
+          <input type="tel" name="phone" placeholder="Phone Number" required>
+        </div>
+        <div class="field">
+          <textarea name="messgae" placeholder="Message" required></textarea>
+        </div>
+        <input type="submit" name="submit" class="button" value="Send">
+        <input type="hidden" name="hidden" value=1>
+      </form>
+    </div>
   </main>
 </div>
 
