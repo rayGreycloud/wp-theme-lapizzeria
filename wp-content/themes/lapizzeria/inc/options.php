583fd4be04ef7e502f3ed1abd106828f996417e8
rayGreycloud <rayGreycloud@gmail.com>
2017-10-04 13:11:46 -0700

    Add reservations submenu page to options menu page

--- wp-content/themes/lapizzeria/inc/options.php
+++ wp-content/themes/lapizzeria/inc/options.php
@@ -2,12 +2,17 @@
 
 function lapizzeria_options() {
   add_menu_page( 'La Pizzeria', 'La Pizzeria Options', 'administrator', 'lapizzeria_options', 'lapizzeria_adjustments', '', 20 );
+
+  add_submenu_page( 'lapizzeria_options', 'Reservations', 'Reservations', 'administrator', 'lapizzeria_reservations', 'lapizzeria_reservations');
 }
 add_action('admin_menu', 'lapizzeria_options');
 
 function lapizzeria_adjustments() {
-  echo "Hola!";
+  echo "Hola from Adjustments!";
 }
 
+function lapizzeria_reservations() {
+  echo "Reservations Here!";
+}
 
  ?>
