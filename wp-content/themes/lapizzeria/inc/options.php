5a03977fed95b305ace73f2694eddb149a4c4c04
rayGreycloud <rayGreycloud@gmail.com>
2017-10-04 13:41:00 -0700

    Print form submissions into wordpress backend

--- wp-content/themes/lapizzeria/inc/options.php
+++ wp-content/themes/lapizzeria/inc/options.php
@@ -8,11 +8,43 @@ function lapizzeria_options() {
 add_action('admin_menu', 'lapizzeria_options');
 
 function lapizzeria_adjustments() {
-  echo "Hola from Adjustments!";
+// For option adjustments
 }
 
-function lapizzeria_reservations() {
-  echo "Reservations Here!";
-}
+function lapizzeria_reservations() { ?>
 
- ?>
+  <div class="wrap">
+    <h1>Reservations</h1>
+    <table class="wp-list-table widefat striped">
+      <thead>
+        <tr>
+          <th class="manage-column">ID</th>
+          <th class="manage-column">Name</th>
+          <th class="manage-column">Reservation Date</th>
+          <th class="manage-column">Email</th>
+          <th class="manage-column">Phone</th>
+          <th class="manage-column">Message</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <?php
+        global $wpdb;
+        $table = $wpdb->prefix . 'reservations';
+        $reservations = $wpdb->get_results("SELECT * FROM $table", ARRAY_A);
+        foreach($reservations as $reservation): ?>
+          <tr>
+            <td><?php echo $reservation['id']; ?></td>
+            <td><?php echo $reservation['name']; ?></td>
+            <td><?php echo $reservation['date']; ?></td>
+            <td><?php echo $reservation['email']; ?></td>
+            <td><?php echo $reservation['phone']; ?></td>
+            <td><?php echo $reservation['message']; ?></td>
+          </tr>
+
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
+
+<?php } ?>
