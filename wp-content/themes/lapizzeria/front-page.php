442a16da7d7cde5ccd16b56a6a3b7868d70b47a3
rayGreycloud <rayGreycloud@gmail.com>
2017-10-05 13:10:44 -0700

    Add specialtie section of front page

--- wp-content/themes/lapizzeria/front-page.php
+++ wp-content/themes/lapizzeria/front-page.php
@@ -13,7 +13,34 @@
 </div>
 
 <div class="main-content container">
-  <main class="text-center content-text clear">
+  <main class="container-grid clear">
+    <h2 class="primary-text text-center">Our Specialties</h2>
+
+    <?php $args = array(
+      'posts_per_page' => 3,
+      'post_type' => 'specialties',
+      'category_name'  => 'pizzas',
+      'orderby' => 'rand'
+    );
+    $specialties = new WP_Query($args);
+    while($specialties->have_posts()): $specialties->the_post(); ?>
+
+    <div class="specialty columns1-3">
+      <div class="specialty-content">
+        <?php the_post_thumbnail('specialty-portrait'); ?>
+        <div class="information">
+          <?php the_title('<h3>', '</h3>'); ?>
+          <?php the_content(); ?>
+          <p class="price">$ <?php the_field('price'); ?></p>
+          <a href="<?php the_permalink(); ?>" class="button primary">read more</a>
+        </div>
+      </div>
+    </div>
+
+
+    <?php endwhile; wp_reset_postdata();
+     ?>
+
 
   </main>
 </div>
