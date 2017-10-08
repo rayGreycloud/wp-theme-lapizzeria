81792fc873342ecad67517c3bf80f96f813e7bb8
rayGreycloud <rayGreycloud@gmail.com>
2017-10-08 12:20:55 -0700

    Add option fields for location, zoom and apikey

--- wp-content/themes/lapizzeria/inc/options.php
+++ wp-content/themes/lapizzeria/inc/options.php
@@ -9,9 +9,9 @@ add_action('admin_menu', 'lapizzeria_options');
 
 function lapizzeria_settings() {
   register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_latitude');
-  // register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_longitude');
-  // register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_zoom');
-  // register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_');
+  register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_longitude');
+  register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_zoom');
+  register_setting('lapizzeria_options_gmaps', 'lapizzeria_gmap_apikey');
 }
 add_action('init', 'lapizzeria_settings');
 
@@ -29,6 +29,24 @@ function lapizzeria_adjustments() { ?>
            <th scope="row">Latitude: </th>
            <td>
              <input type="text" name="lapizzeria_gmap_latitude" value="<?php echo esc_attr(get_option('lapizzeria_gmap_latitude'));  ?>" />
+           </td>
+         </tr>
+         <tr valign="top">
+           <th scope="row">Longitude: </th>
+           <td>
+             <input type="text" name="lapizzeria_gmap_longitude" value="<?php echo esc_attr(get_option('lapizzeria_gmap_longitude'));  ?>" />
+           </td>
+         </tr>
+         <tr valign="top">
+           <th scope="row">Zoom Level: </th>
+           <td>
+             <input type="number" min='12' max='21' name="lapizzeria_gmap_zoom" value="<?php echo esc_attr(get_option('lapizzeria_gmap_zoom'));  ?>" />
+           </td>
+         </tr>
+         <tr valign="top">
+           <th scope="row">API Key: </th>
+           <td>
+             <input type="text" name="lapizzeria_gmap_apikey" value="<?php echo esc_attr(get_option('lapizzeria_gmap_apikey'));  ?>" />
            </td>
          </tr>
        </table>
